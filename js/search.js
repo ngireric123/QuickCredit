53eda7c3952daf1e423f7551097bcfcde021da41
ngireric123 <38130305+ngireric123@users.noreply.github.com>
2019-04-22 12:23:19 +0200

    Delete search.js

--- js/search.js
+++
@@ -1,6 +0,0 @@
-document.getElementById("myInput").addEventListener("search", myFunction);
-
-function myFunction() {
-  var x = document.getElementById("myInput");
-  document.getElementById("demo").innerHTML = "" 
-}
